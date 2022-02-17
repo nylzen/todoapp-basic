99e5b25b708855a9318ad6e3904e3992d3da2ff2
Nylzen <tugoresn@gmail.com>
2022-02-17 12:40:12 -0300

    todoapp sin local storage

---
+++ index.js
@@ -0,0 +1,30 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todos = document.getElementById("todos");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addTodo();
+});
+
+function addTodo() {
+  const todoText = input.value;
+
+  if (todoText) {
+    const todoEl = document.createElement("li");
+    todoEl.innerText = todoText;
+
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+    });
+
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      todoEl.remove();
+      localSt();
+    });
+
+    todos.appendChild(todoEl);
+    input.value = "";
+  }
+}
